2ec5b5fd468126b31aed36ed9cd69ffc0c35447e
FedeBon <bonbonfede@libero.it>
2024-01-04 20:48:51 +0100

    generei

--- mimmiApp/src/app/services/global-variables.service.ts
+++ mimmiApp/src/app/services/global-variables.service.ts
@@ -6,7 +6,7 @@ import { User } from '../DTO/User';
 })
 export class GlobalVariablesService {
 
-  bookGenres = ["Thriller", "Fantasy", "Fantascienza", "Narrativa contemporanea", "Romanzi Rosa", "Classici", "Saggi", "Crescita Personale", "Psicologia"]
+  bookGenres = ["Sport", "Thriller", "Fantasy", "Giallo", "Racconti","Fotografia-arte", "Autobiografia","Storico","New Age", "Poesie","Manuale", "Fantascienza", "Narrativa contemporanea","Romanzo", "Romanzi Rosa", "Classici","Spiritualità", "Saggi","Saggio storico", "Saggio storia Politica", "Crescita Personale", "Psicologia"]
   goodCondiction = [0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5]
 
   userInfos: User[] = [/*{
