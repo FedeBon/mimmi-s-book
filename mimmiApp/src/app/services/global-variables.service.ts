076015a24948b8ccf41919715703bf71a402c351
FedeBon <bonbonfede@libero.it>
2024-01-11 14:55:12 +0100

    correzione

--- mimmiApp/src/app/services/global-variables.service.ts
+++ mimmiApp/src/app/services/global-variables.service.ts
@@ -9,11 +9,11 @@ export class GlobalVariablesService {
   bookGenres = ["Antropologia", "Autobiografia", "Classici", "Crescita Personale", "Fantascienza", "Fantasy", "Fotografia-arte", "Giallo","Horror", "Letteratura", "Manuale", "Narrativa contemporanea", "New Age", "Poesie", "Psicologia", "Racconti", "Romanzo", "Romanzi Rosa", "Saggi", "Saggio storico", "Saggio storia Politica", "Sociologia", "Spiritualità", "Sport", "Storico", "Thriller"]
   goodCondiction = [0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5]
 
-  userInfos: User[] = [{
+  userInfos: User[] = [/*{
     "nickname": "Admin",
     "password": "Admin",
     "role": "Admin"
-  }]
+  }*/]
   
   constructor() { }
 
